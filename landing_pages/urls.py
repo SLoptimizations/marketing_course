acc2a18ca73bd85b6b38e62bfe7542602c3ac2d9
ShovalLevi22 <shovi22@gmail.com>
2020-05-12 00:55:35 +0300

    Initial commit

---
+++ landing_pages/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from landing_pages import views
+
+urlpatterns = [
+    # path('', views.AboutView.as_view(), name='about'),
+    path('', views.register, name='about'),
+    path('shein', views.shein_view, name='shein'),
+    path('nespresso', views.nespresso_view, name='nespresso'),
+    path('yoga_guide', views.VideoPageView.as_view(), name='all_video'),
+    path('unsubscribe', views.UnsubscribeView.as_view(), name='unsubscribe')
+]
